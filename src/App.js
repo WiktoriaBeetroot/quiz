881aec562e6c72917523b985c8d6cef382bc1fa3
Victoriia <viktoriia.paster@beetroot.se>
2025-03-27 16:56:46 +0200

    ADD | fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { useEffect, useReducer } from 'react';
 import { Header } from './Header';
 import { Main } from './Main';
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading'
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'dataRecieved': 
+    return {
+      ...state,
+      questions: action.payload,
+      status: 'ready'
+    };
+    case 'dataFailed' :
+      return {
+        ...state,
+        status: 'error'
+      }
+    default: 
+      throw new Error('Action is undefined')
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = 'http://localhost:8000/questions';
+
+      try {
+        const response = await fetch(url);
+  
+        if (!response.ok) {
+          throw new Error('Error while fetching data')
+        }
+  
+        const data = await response.json();
+        dispatch({type: 'dataRecieved', payload: data})
+      }catch(error) {
+        dispatch({type: 'dataFailed'})
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div class="app">
+    <div className="app">
       <Header />
 
       <Main>
