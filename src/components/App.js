fe98feb9276510ea4a9b8375907c3dc43baf9f94
Victoriia <viktoriia.paster@beetroot.se>
2025-04-01 18:31:39 +0300

    ADD | handling answer functionality

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 }
 
 function reducer(state, action) {
@@ -32,13 +34,24 @@ function reducer(state, action) {
         ...state,
         status: 'active'
       }
+    case 'setAnswer' :
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points: 
+        action.payload === question.correctOption 
+        ? state.points + question.points
+        : state.points
+      }
     default: 
       throw new Error('Action is undefined')
   }
 }
 
 export default function App() {
-  const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+  const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
@@ -71,7 +84,7 @@ export default function App() {
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
         {status === 'ready' && <MainScreen numQuestions={ numQuestions } dispatch={ dispatch }/>}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && <Question question={questions[index]}  dispatch={dispatch} answer={answer}/>}
       </Main>
     </div>
   )
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,7 +1,16 @@
-export const Options = ({question}) => {
+export const Options = ({question, dispatch, answer}) => {
+    const hasAnswer = answer !== null;
     return (
         <div className="options">
-            {question.options.map((item) => <button className="btn btn-option">{item}</button>)}
+            {question.options.map((item, index) => 
+                <button 
+                    key={item}
+                    className={`btn btn-option ${index === answer ? 'answer' : '' } ${hasAnswer ? index === question.correctOption ? 'correct' : 'wrong' : ''}`} 
+                    onClick={() => dispatch({type: 'setAnswer', payload: index})}
+                    disabled={hasAnswer}>
+                    {item}
+                </button>)
+            }
         </div>
     )
 }
